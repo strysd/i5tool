99443fbd04bfb2e1c27b75e6390e1c6a17b27a88
Satoru Yoshida <ramat@ram.ne.jp>
2011-05-17 21:24:57 +0900

    i5Conn and i5Pgm are debugged and fixed.  Add testpgm and ztestr for sample.

--- i5Conn.php
+++ i5Conn.php
@@ -1,133 +1,121 @@
 <?php
 class i5Conn{
 
-	/**
-	 * resource from i5_connect
-	 * @var Connection
-	 */
-	protected static $conn;
+    /**
+     * resource from i5_connect
+     * @var Connection
+     */
+    protected static $conn;
 
-	/**
-	 * options for i5_connect
-	 * @var array
-	 */
-	private static $options = array(
-		//TODO define Your options for i5_connect
-	);
+    /**
+     * options for i5_connect
+     * @var array
+     */
+    protected static $options = array(
+        //TODO define Your options for i5_connect
+        I5_OPTIONS_CODEPAGEFILE => '/usr/local/zendsvr/etc/jp_1399.cpg'
+    );
 
-	/**
-	 * basic parameters
-	 * @var object
-	 */
-	private static $basic;
+    /**
+     * basic parameters
+     * @var string
+     */
+    private static $host;
+    private static $user;
+    private static $pass;
 
-	/**
-	 * if persistent connection
-	 * @var boolean
-	 */
-	protected static $persistent;
+    /**
+     * if persistent connection
+     * @var boolean
+     */
+    protected static $persistent;
 
-	/**
-	 * Open connection
-	 * @throws Exception
-	 */
-	function __construct() {
-		if(!isset(self::$conn) || !is_resource(self::$conn)){
-			if(!isset(self::$basic) || !is_object(self::$basic)){
-				require_once 'Zend/Config.php';
-				self::$basic = new Zend_Config_Ini('setting.ini', 'conn');
-				if(!isset(self::$basic->host) ||
-				   !isset(self::$basic->user) ||
-				   !isset(self::$basic->pass) ){
-					throw new Exception('Basic parameters for i5_connect are undefined');
-				}
-			}
-			self::$persistent = self::$basic->persistent;
-			if(self::persistent){
-				$ret = i5_pconnect(self::$basic->host, self::$basic->user,
-								   self::$basic->pass, self::$options);
-			} else {
-				$ret =  i5_connect(self::$basic->host, self::$basic->user,
-								   self::$basic->pass, self::$options);
-			}
-			if(!$ret){
-				throw new Exception(i5_errormsg(), i5_errno());
-			}
-			self::$conn = $ret;
-		}
-	}
+    /**
+     * Open connection
+     * @throws Exception
+     */
+    function __construct() {
+        if(!isset(self::$conn) || !is_resource(self::$conn)){
+            if(!isset(self::$host)){
+                require_once 'Zend/Config/Ini.php';
+                $config = new Zend_Config_Ini('setting.ini', 'conn');
+                if(!isset($config->host) ||
+                   !isset($config->user) ||
+                   !isset($config->pass) ){
+                    throw new Exception('Basic parameters for i5_connect are undefined');
+                }
+                self::$host = $config->host;
+                self::$user = $config->user;
+                self::$pass = $config->pass;
+                self::$persistent = $config->persistent;
+            }
+            if(self::$persistent){
+                $connect = i5_pconnect(self::$host, self::$user,
+                                       self::$pass, self::$options);
+            } else {
+                $connect =  i5_connect(self::$host, self::$user,
+                                       self::$pass, self::$options);
+            }
+            if(!$connect){
+                throw new Exception(i5_errormsg(), i5_errno());
+            }
+            self::$conn = $connect;
+        }
+    }
 
-	/**
-	 * Close non-persistent connection
-	 * @see pclose() if You need to close persistent connection
-	 * @throws Exception
-	 */
-	function __destruct() {
-		if(self::$persistent){
-			return;
-		}
-		if(isset(self::$conn) && is_resource(self::$conn)){
-			$ret = i5_close(self::$conn);
-			if(!$ret){
-				throw new Exception(i5_errormsg(), i5_errno());
-			}
-			unset(self::$conn);
-		}
-	}
+    /**
+     * Close non-persistent connection
+     * @see pclose() if You need to close persistent connection
+     * @throws Exception
+     */
+    function __destruct() {
+        if(self::$persistent){
+            return;
+        }
+        if(isset(self::$conn) && is_resource(self::$conn)){
+            $ret = i5_close(self::$conn);
+            if(!$ret){
+                throw new Exception(i5_errormsg(), i5_errno());
+            }
+            $this->_clearStatics();
+        }
+    }
 
-	/**
-	 * Close persistent connection
-	 * @throws Exception
-	 */
-	function pclose() {
-		if(!self::$persistent){
-			throw new Exception('Can not use this function for non-persistent connection');
-		}
-		if(isset(self::$conn) && is_resource(self::$conn)){
-			$ret = i5_pclose(self::$conn);
-			if(!$ret){
-				throw new Exception(i5_errormsg(), i5_errno());
-			}
-		}
-	}
+    protected function _clearStatics() {
+        self::$conn = null;
+        self::$host = null;
+        self::$user = null;
+        self::$pass = null;
+        self::$persistent = null;
+    }
 
-	/**
-	 * Check if new connection
-	 * @throws Exception
-	 * @return string/int
-	 * @see i5_get_property()
-	 */
-	function is_newConnection() {
-		if(!self::$persistent){
-			throw new Exception('Can not use this function for non-persistent connection');
-		}
-		return i5_get_property(I5_NEW_CONNECTION, self::$conn);
-	}
-	/**
-	 * Converts input parameter
-	 * @param array $in input parameters
-	 * @param array $keys parameters to convert
-	 * @see i5Pgm
-	 * @see i5Command
-	 */
-	protected function inConvert(&$in, $keys = array()){
-		//TODO Example for Japanese
-		foreach ($keys as $key) {
-			$in[$key] = mb_convert_encoding($in[$key], 'sjis-win', 'UTF-8');
-		}
-	}
+    /**
+     * Close persistent connection
+     * @throws Exception
+     */
+    function pclose() {
+        if(!self::$persistent){
+            throw new Exception('Can not use this function for non-persistent connection');
+        }
+        if(isset(self::$conn) && is_resource(self::$conn)){
+            $ret = i5_pclose(self::$conn);
+            if(!$ret){
+                throw new Exception(i5_errormsg(), i5_errno());
+            }
+            $this->_clearStatics();
+        }
+    }
 
-	/**
-	 * Converts output parameter
-	 * @param array $out output parameters
-	 * @param array $keys parameters to convert
-	 * @see i5Pgm
-	 * @see i5Command
-	 */
-	protected function outConvert(&$out, $keys = array()){
-		//TODO Example for Japanese
-		foreach ($keys as $key) {
-			$out[$key] = mb_convert_encoding($out[$key], 'UTF-8', 'sjis-win');
-		}
-	}
+    /**
+     * Check if new connection
+     * @throws Exception
+     * @return string/int
+     * @see i5_get_property()
+     */
+    function is_newConnection() {
+        if(!self::$persistent){
+            throw new Exception('Can not use this function for non-persistent connection');
+        }
+        return i5_get_property(I5_NEW_CONNECTION, self::$conn);
+    }
 }
--- i5Pgm.php
+++ i5Pgm.php
@@ -2,80 +2,73 @@
 require_once './i5Conn.php';
 class i5Pgm extends i5Conn{
 
-	/**
-	 * Program id array
-	 * @var array
-	 */
-	protected static $pgmid = array();
+    /**
+     * resource from i5_program_prepare
+     * @var Program
+     */
+    protected $pgm;
 
-	/**
-	 * Open program resource
-	 * @param string/int $id Program id as You like. For example, 'summerizeTable'
-	 * @param string $pgm real name of libray and program. For example, 'YOURLIB/YOURPGM'
-	 * @param array $description defines input/output parameters
-	 * @throws Exception
-	 */
-	function __construct($id, $pgm, $description = array()) {
-		if(!isset(self::$pgmid[$id]) || !is_resource(self::$pgmid[$id])){
-			if(!isset(parent::$conn)){
-				//TODO confirm we can use
-				parent::__construct();
-			}
-			$ret = i5_program_prepare($pgm, $description, parent::$conn);
-			if(!$ret){
-				throw new Exception(i5_errormsg(), i5_errno());
-			}
-			self::$pgmid[$id] = $ret;
-		}
-	}
+    /**
+     * Open program resource
+     * @param string $pgm real name of libray and program. For example, 'YOURLIB/YOURPGM'
+     * @param array $description defines input/output parameters
+     * @throws Exception
+     */
+    function __construct($pgm, $description = array()) {
+        if(!isset($this->pgm) || !is_resource($this->pgm)){
+            if(!isset(self::$conn)){
+                try {
+                    parent::__construct();
+                } catch (Exception $e) {
+                    throw new Exception($e);
+                }
+            }
+            $program = i5_program_prepare($pgm, $description, self::$conn);
+            if(!$program){
+                throw new Exception(i5_errormsg(), i5_errno());
+            }
+            $this->pgm = $program;
+        }
+    }
 
-	function __destruct() {
-		foreach (self::$pgmid as $id) {
-			$this->close($id);
-		}
-		//TODO confirm we can use
-		parent::__destruct();
-	}
+    function __destruct() {
+        $this->close();
+        parent::__destruct();
+    }
 
-	/**
-	 * Close program resource
-	 * @param string/int $id Program id as You like. For example, 'summerizeTable'
-	 * @throws Exception
-	 */
-	function close($id) {
-		if(isset(self::$pgmid[$id]) && is_resource(self::$pgmid[$id])){
-			$ret = i5_program_close(self::$pgmid[$id]);
-			if(!$ret){
-				throw new Exception(i5_errormsg(), i5_errno());
-			}
-			unset(self::$pgmid[$id]);
-		}
-	}
+    /**
+     * Close program resource
+     * @throws Exception
+     */
+    function close() {
+        if(isset($this->pgm) && is_resource($this->pgm)){
+            $ret = i5_program_close($this->pgm);
+            if(!$ret){
+                throw new Exception(i5_errormsg(), i5_errno());
+            }
+            unset($this->pgm);
+        }
+    }
 
-	/**
-	 * Call program
-	 * @param string/int $id Program id as You like. For example, 'summerizeTable'
-	 * @param array $in input parameters
-	 * @param array $out output parameters
-	 * @param array $inConvert input parameters　to convert
-	 * @param array $outConvert output parameters　to convert
-	 * @throws Exception
-	 * @return array
-	 */
-	function call($id, $in = array(), $out = array(), $inConvert = array(), $outConvert = array()) {
-		if(!isset(self::$pgmid[$id]) || !self::$pgmid[$id]){
-			throw new Exception('This program id is not defined');
-		}
-		$this->inConvert($in, $inConvert);
-		$ret = i5_program_call(self::$pgmid[$id], $in, $out);
-		if(!$ret){
-			throw new Exception(i5_errormsg(), i5_errno());
-		}
-		$retValue = array();
-		foreach ($out as $outKey => $outValue) {
-			$retValue[$outValue] = $$outValue;
-		}
-		$this->outConvert($out, $outConvert);
-		return $retValue;
-	}
+    /**
+     * Call program
+     * @param array $in input parameters
+     * @param array $out output parameters
+     * @throws Exception
+     * @return array
+     */
+    function call($in = array(), $out = array()) {
+        if(!isset($this->pgm) || !$this->pgm){
+            throw new Exception('Program is not defined');
+        }
+        $ret = i5_program_call($this->pgm, $in, $out);
+        if(!$ret){
+            throw new Exception(i5_errormsg(), i5_errno());
+        }
+        $retValue = array();
+        foreach ($out as $outKey => $outValue) {
+            $retValue[$outValue] = $$outValue;
+        }
+        return $retValue;
+    }
 }
